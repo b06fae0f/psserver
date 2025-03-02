166251f57d3353c663957f734246221a9b3ebc6a
b06fae0f <166984764+b06fae0f@users.noreply.github.com>
2025-03-02 15:52:06 +0200

    Create genmimetypes.ps1

---
+++ genmimetypes/genmimetypes.ps1
@@ -0,0 +1,34 @@
+# Output JSON file
+$Outfile = ".\mimetypes.json"
+
+# Hash table for the Mime Types
+$MimeTypes = @{}
+
+# Mime types URI
+$MimeTypesURI = "https://developer.mozilla.org/en-US/docs/Web/HTTP/MIME_types/Common_types"
+
+try {
+    # Get the HTML content
+    $Response = Invoke-WebRequest -Uri $MimeTypesURI
+
+    # Get the HTML table with the Mime types
+    $MimeTypesTable = $Response.ParsedHtml.body.getElementsByClassName("table-container")[0].nextSibling
+    
+    # Get table body
+    $TBODY = $MimeTypesTable.getElementsByTagName("tbody")[0] 
+    
+    # Get Mime types from each row
+    $TBODY.getElementsByTagName("tr") | ForEach-Object -Process {
+        $TD = $_.getElementsByTagName("td")
+        $Extension = $TD[0].getElementsByTagName("code")[0].innerText
+        $MimeType = $TD[2].getElementsByTagName("code")[0].innerText
+        $MimeTypes[$Extension] = $MimeType
+    }
+    
+    # Save Mime types as a JSON file
+    [System.IO.File]::WriteAllLines($Outfile, ($MimeTypes | ConvertTo-JSON), [System.Text.UTF8Encoding]::new($false))
+    
+    Write-Host "Mime types file was created successfuly."
+} catch {
+    Write-Host "Error: $_";
+}
