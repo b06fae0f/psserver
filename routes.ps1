fbc6c798efecf62b729e846f8615d604a4d1fe70
b06fae0f <166984764+b06fae0f@users.noreply.github.com>
2025-03-24 08:47:18 +0200

    Create routes.ps1
    routes file added

---
+++ routes.ps1
@@ -0,0 +1,14 @@
+switch -RegEx ($request.Url.AbsolutePath.TrimEnd("/")) {
+	{$_ -eq "/hello"} {
+		$response.ContentType = "text/plain; charset=UTF-8"
+		"Hello, World!"
+	}
+	"^/sayhi/([^/]+)$" {
+		$name = $Matches[1]
+		$response.ContentType =  "text/plain; charset=UTF-8"
+        "Hi, $name!"
+	}
+	default {
+		$RouteNotFound = $true
+	}
+}
